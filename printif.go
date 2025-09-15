1e5964450dc3123a8c1c1f83d3bf34a7597b6934
nilsgtcepo <nilsgtcepo@gmail.com>
2025-09-15 16:01:40 +0200

    j'ai rien capté au contrôle

---
+++ printif.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func PrintIf(str string) string {
+	if str == (\\len(str >= 3)) {
+		return "G \n"
+	} else {
+		return "invalid input \n"
+	}
+}
+
+func main() {
+	fmt.Print(PrintIf("abcdefz"))
+	fmt.Print(PrintIf("abc"))
+	fmt.Print(PrintIf(""))
+	fmt.Print(PrintIf("14"))
+}
